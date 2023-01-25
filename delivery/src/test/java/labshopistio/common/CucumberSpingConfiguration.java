e641e5111386cf6fe433c468e70982474ffbb9dd
경호선 <hossun73@gmail.com>
2023-01-25 17:28:04 +0900

    commit

---
+++ delivery/src/test/java/labshopistio/common/CucumberSpingConfiguration.java
@@ -0,0 +1,12 @@
+package labshopistio.common;
+
+
+import labshopistio.DeliveryApplication;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@SpringBootTest(classes = { DeliveryApplication.class })
+public class CucumberSpingConfiguration {
+    
+}
---
+++ inventory/src/test/java/labshopistio/main/TestMain.java
@@ -0,0 +1,13 @@
+package labshopistio.main;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin={"pretty","html:target/cucumber"},
+                features = "src/test/resources/features",
+                extraGlue="labshopistio/common")
+public class TestMain {
+    
+}
